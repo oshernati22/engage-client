94cf46c59bab327c9500a873caa3c01ea6a80ec6
Osher <oshernati22@gmail.com>
2022-02-01 14:22:05 +0200

    remove StrictMode

--- src/index.js
+++ src/index.js
@@ -3,13 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// remove StrictMode check if this reson of heroku broken
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
